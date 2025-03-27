292ee9dfce55033a99d6062702e490ab5844a7b9
X1ngFeng <jimmy45362@gmail.com>
2025-03-27 16:01:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = require("@actions/core");
-const webhook_1 = require("@slack/webhook");
 const axios_1 = require("axios");
 const qs = require("querystring");
+
 (async () => {
     var _a;
     // Validate parameters
@@ -11,7 +11,7 @@ const qs = require("querystring");
         "product-id",
         "schedule-id",
         "seat-id",
-        "slack-incoming-webhook-url",
+        "discord-incoming-webhook-url",  // 修改參數名稱為 Discord Webhook URL
     ].map((name) => {
         const value = core.getInput(name);
         if (!value) {
@@ -20,7 +20,19 @@ const qs = require("querystring");
         return value;
     });
     const message = (_a = core.getInput("message")) !== null && _a !== void 0 ? _a : "티켓사세요";
-    const webhook = new webhook_1.IncomingWebhook(webhookUrl);
+
+    // 發送 Discord 通知的函式
+    const sendDiscordNotification = async (content: string) => {
+        try {
+            await axios_1.default.post(webhookUrl, {
+                content: content,
+            });
+        } catch (error) {
+            console.error("Failed to send Discord notification:", error);
+        }
+    };
+
+    // 向 Melon Ticket API 發送請求
     const res = await axios_1.default({
         method: "POST",
         url: "https://ticket.melon.com/tktapi/product/seatStateInfo.json",
@@ -35,13 +47,16 @@ const qs = require("querystring");
             selectedGradeVolume: 1,
         }),
     });
-    // tslint:disable-next-line
+    
+    // Debug 訊息
     console.log("Got response: ", res.data);
+
+    // 如果票券可用，發送 Discord 通知
     if (res.data.chkResult) {
         const link = `http://ticket.melon.com/performance/index.htm?${qs.stringify({
             prodId: productId,
         })}`;
-        await webhook.send(`${message} ${link}`);
+        await sendDiscordNotification(`${message} ${link}`);
     }
 })().catch((e) => {
     console.error(e.stack); // tslint:disable-line
